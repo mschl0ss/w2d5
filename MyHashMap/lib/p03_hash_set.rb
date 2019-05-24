f7ef9f9c58cfd35c1edcacefc9cf62974f2a0a34
mschl0ss <mr.schlossberg@gmail.com>
2019-05-24 16:17:16 -0700

    Add p03_hash_set

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,12 +7,23 @@ class HashSet
   end
 
   def insert(key)
+    @store[key.hash % @store.length] << key
+    @count += 1
+    resize! if @count >= @store.length
   end
 
   def include?(key)
+    @store[key.hash % @store.length].each do |ele|
+      return true if ele == key
+    end
+    false
   end
 
   def remove(key)
+    if include?(key)
+      @store[key.hash % @store.length].delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +37,13 @@ class HashSet
   end
 
   def resize!
+    h = HashSet.new(@store.length * 2)
+    @store.each do |bucket|
+      bucket.each do |key|
+        h.insert(key)
+      end
+    end
+    @store = h.store
+    @count = 0
   end
 end
